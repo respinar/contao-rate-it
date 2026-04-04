c90a4f70dca2f5676d5e1b8acb856c76ae01d3e5
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-04 13:45:59 +0330

    add ecs and rector

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\ValueObject\PhpVersion;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        __DIR__.'/contao',
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_83);
+
+    $rectorConfig->sets([
+        SetList::PHP_83,
+        SetList::TYPE_DECLARATION,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SymfonySetList::SYMFONY_64,
+    ]);
+};
