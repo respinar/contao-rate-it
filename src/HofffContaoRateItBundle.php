dfdbe19db55bf4bcad2e718c10342532a53c84f8
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-04-05 23:54:00 +0330

    update

--- src/HofffContaoRateItBundle.php
+++ src/HofffContaoRateItBundle.php
@@ -14,10 +14,25 @@
  * @filesource
  */
 
+declare(strict_types=1);
+
 namespace Hofff\Contao\RateIt;
 
+use Hofff\Contao\RateIt\DependencyInjection\Compiler\RemoveInactiveListenersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class HofffContaoRateItBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RemoveInactiveListenersPass());
+    }
 }
