f75803105c4e6076ae05e5b909be9a34d3bce0d1
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-16 22:56:54 +0330

    Limit ECS paths to contao and src directories

--- ecs.php
+++ ecs.php
@@ -9,6 +9,7 @@ return ECSConfig::configure()
     ->withSets([SetList::CONTAO])
     // Adjust the configuration according to your needs.
     ->withPaths([
-        __DIR__.'/',
+        __DIR__.'/contao',
+        __DIR__.'/src',
     ])
 ;
