69968cca5b95d52df197699cf816171f4684f07a
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-17 23:28:57 +0330

    Add strict types and remove redundant constructor

--- src/Frontend/RateItCE.php
+++ src/Frontend/RateItCE.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of hofff/contao-rate-it.
  *
@@ -13,7 +15,6 @@
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
-
 namespace Hofff\Contao\RateIt\Frontend;
 
 /**
@@ -21,14 +22,6 @@ namespace Hofff\Contao\RateIt\Frontend;
  */
 class RateItCE extends RateItHybrid
 {
-
-    /**
-     * Initialize the controller
-     */
-    public function __construct($objElement)
-    {
-        parent::__construct($objElement);
-    }
 
     protected function getType() : string
     {
