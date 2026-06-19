45ca11d3f4dc2556d1c8f01dc5a6ec7c5cf0a193
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 09:04:03 +0330

    Fix code style inconsistencies in MigrateCommand

--- src/Console/Command/MigrateCommand.php
+++ src/Console/Command/MigrateCommand.php
@@ -3,12 +3,12 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
  * @copyright  2019-2020 hofff.com.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
@@ -17,19 +17,15 @@ declare(strict_types=1);
 namespace Hofff\Contao\RateIt\Console\Command;
 
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver\Statement;
-use Doctrine\DBAL\ForwardCompatibility\Result as ForwardCompatibilityResult;
 use Doctrine\DBAL\Result;
 use Hofff\Contao\RateIt\Rating\RatingTypes;
 use Hofff\Contao\RateIt\Rating\SourceInformation;
-use PDO;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use function time;
 
 #[AsCommand(
     name: 'hofff-rate-it:migrate',
@@ -45,29 +41,18 @@ final class MigrateCommand extends Command
     {
         parent::__construct();
 
-        $this->connection  = $connection;
+        $this->connection = $connection;
         $this->ratingTypes = $ratingTypes;
     }
 
-    protected function configure() : void
+    protected function configure(): void
     {
-        $this->addArgument(
-            'task',
-            InputArgument::OPTIONAL,
-            'Decide which migration task should be run',
-            'article-to-page'
-        );
+        $this->addArgument('task', InputArgument::OPTIONAL, 'Decide which migration task should be run', 'article-to-page',);
 
-        $this->addOption(
-            'position',
-            'p',
-            InputOption::VALUE_REQUIRED,
-            'Position of the rating being added to a page',
-            'before'
-        );
+        $this->addOption('position', 'p', InputOption::VALUE_REQUIRED, 'Position of the rating being added to a page', 'before',);
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) : int
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $task = $input->getArgument('task');
 
@@ -76,11 +61,11 @@ final class MigrateCommand extends Command
                 return $this->migrateArticlesToPages($input);
 
             default:
-                throw new \InvalidArgumentException(sprintf('Task "%s" is not supported.', $task));
+                throw new \InvalidArgumentException(\sprintf('Task "%s" is not supported.', $task));
         }
     }
 
-    private function migrateArticlesToPages(InputInterface $input) : int
+    private function migrateArticlesToPages(InputInterface $input): int
     {
         $unratedPagesWithArticleRatings = $this->getUnratedPagesWithArticleRatings();
 
@@ -96,22 +81,22 @@ final class MigrateCommand extends Command
     private function getUnratedPagesWithArticleRatings(): Result
     {
         $sql = <<<'SQL'
-SELECT
-       p.`id` AS 'pageId'
-FROM `tl_page` p
-INNER JOIN `tl_article` a               ON a.`pid` = p.`id`
-LEFT JOIN `tl_rateit_items` ri_page     ON p.`id` = ri_page.`rkey`    AND ri_page.`typ` = 'page'
-INNER JOIN `tl_rateit_items` ri_article ON a.`id` = ri_article.`rkey` AND ri_article.`typ` = 'article'
-WHERE
-    ri_page.`id` IS NULL
-    AND ri_article.`id` IS NOT NULL
-GROUP BY p.id
-SQL;
+            SELECT
+                   p.`id` AS 'pageId'
+            FROM `tl_page` p
+            INNER JOIN `tl_article` a               ON a.`pid` = p.`id`
+            LEFT JOIN `tl_rateit_items` ri_page     ON p.`id` = ri_page.`rkey`    AND ri_page.`typ` = 'page'
+            INNER JOIN `tl_rateit_items` ri_article ON a.`id` = ri_article.`rkey` AND ri_article.`typ` = 'article'
+            WHERE
+                ri_page.`id` IS NULL
+                AND ri_article.`id` IS NOT NULL
+            GROUP BY p.id
+            SQL;
 
         return $this->connection->executeQuery($sql);
     }
 
-    private function createRateItItem($pageId, string $position) : void
+    private function createRateItItem($pageId, string $position): void
     {
         $sourceInformation = $this->ratingTypes->sourceInformation('page', (int) $pageId);
         if (!$sourceInformation instanceof SourceInformation) {
@@ -119,12 +104,12 @@ SQL;
         }
 
         $arrSet = [
-            'rkey'         => $pageId,
-            'tstamp'       => time(),
-            'typ'          => 'page',
-            'createdat'    => time(),
-            'title'        => $sourceInformation->title(),
-            'active'       => '1',
+            'rkey' => $pageId,
+            'tstamp' => \time(),
+            'typ' => 'page',
+            'createdat' => \time(),
+            'title' => $sourceInformation->title(),
+            'active' => '1',
             'parentstatus' => $sourceInformation->parentStatus(),
         ];
 
@@ -132,17 +117,17 @@ SQL;
         $this->connection->update('tl_page', ['addRating' => '1', 'rateit_position' => $position], ['id' => $pageId]);
     }
 
-    private function migrateArticleRatings() : void
+    private function migrateArticleRatings(): void
     {
         $sql = <<<'SQL'
 
-UPDATE
-    tl_rateit_ratings r
-INNER JOIN tl_rateit_items ri_article ON ri_article.id=r.pid AND ri_article.typ='article'
-INNER JOIN tl_article a on a.id=ri_article.rkey
-INNER JOIN tl_rateit_items ri_page ON ri_page.rkey = a.pid AND ri_page.typ='page'
-SET r.pid=ri_page.id;
-SQL;
+            UPDATE
+                tl_rateit_ratings r
+            INNER JOIN tl_rateit_items ri_article ON ri_article.id=r.pid AND ri_article.typ='article'
+            INNER JOIN tl_article a on a.id=ri_article.rkey
+            INNER JOIN tl_rateit_items ri_page ON ri_page.rkey = a.pid AND ri_page.typ='page'
+            SET r.pid=ri_page.id;
+            SQL;
 
         $this->connection->executeQuery($sql);
         $this->connection->executeQuery('UPDATE tl_article SET addRating=\'\'');
