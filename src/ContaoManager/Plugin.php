3c93057b82e42e89a2557baae9c419565835ae5c
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 09:06:01 +0330

    Migrate PHP code style and update annotations

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -3,14 +3,13 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
@@ -20,8 +19,8 @@ namespace Hofff\Contao\RateIt\ContaoManager;
 
 use Contao\CommentsBundle\ContaoCommentsBundle;
 use Contao\CoreBundle\ContaoCoreBundle;
-use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Contao\NewsBundle\ContaoNewsBundle;
@@ -32,14 +31,9 @@ use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Plugin for the Contao Manager.
- *
- * @author Carsten Götzinger
  */
 final class Plugin implements BundlePluginInterface, RoutingPluginInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getBundles(ParserInterface $parser): array
     {
         return [
@@ -49,13 +43,11 @@ final class Plugin implements BundlePluginInterface, RoutingPluginInterface
         ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): ?RouteCollection
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): RouteCollection|null
     {
         return $resolver
-            ->resolve(__DIR__ . '/../../config/routing.yaml')
-            ->load(__DIR__ . '/../../config/routing.yaml');
+            ->resolve(__DIR__.'/../../config/routing.yaml')
+            ->load(__DIR__.'/../../config/routing.yaml')
+        ;
     }
 }
