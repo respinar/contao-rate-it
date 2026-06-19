b0956a3e383f352b31bc93fc776e9ded3929063d
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:23:15 +0330

    Add strict types and clean up config

--- contao/config/config.php
+++ contao/config/config.php
@@ -1,22 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
 use Hofff\Contao\RateIt\Backend\RateItBackend;
 use Hofff\Contao\RateIt\Backend\RateItBackendModule;
-use Hofff\Contao\RateIt\EventListener\Hook\FrontendIntegrationListener;
 use Hofff\Contao\RateIt\Frontend\RateItTopRatingsModule;
 
 /*
@@ -26,13 +26,13 @@ $GLOBALS['BE_MOD']['content'] = array_merge(
     array_slice($GLOBALS['BE_MOD']['content'], 0, -1, true),
     [
         'rateit' => [
-            'callback'   => RateItBackendModule::class,
-            'icon'       => RateItBackend::image('icon'),
+            'callback' => RateItBackendModule::class,
+            'icon' => RateItBackend::image('icon'),
             'stylesheet' => RateItBackend::css('backend'),
             'javascript' => RateItBackend::js('RateItBackend'),
         ],
     ],
-    array_slice($GLOBALS['BE_MOD']['content'], -1, null, true)
+    array_slice($GLOBALS['BE_MOD']['content'], -1, null, true),
 );
 
 /*
@@ -43,9 +43,9 @@ $GLOBALS['FE_MOD']['application']['rateit_top_ratings'] = RateItTopRatingsModule
 /*
  * Default configuration
  */
-$GLOBALS['TL_CONFIG']['rating_count']        = 5;
+$GLOBALS['TL_CONFIG']['rating_count'] = 5;
 $GLOBALS['TL_CONFIG']['rating_textposition'] = 'after';
-$GLOBALS['TL_CONFIG']['rating_listsize']     = 10;
-$GLOBALS['TL_CONFIG']['rating_template']     = 'rateit_default';
-$GLOBALS['TL_CONFIG']['rating_description']  = '%current%/%max% %type% (%count% [Stimme|Stimmen])';
-$GLOBALS['TL_CONFIG']['rating_success']      = '';
+$GLOBALS['TL_CONFIG']['rating_listsize'] = 10;
+$GLOBALS['TL_CONFIG']['rating_template'] = 'rateit_default';
+$GLOBALS['TL_CONFIG']['rating_description'] = '%current%/%max% %type% (%count% [Stimme|Stimmen])';
+$GLOBALS['TL_CONFIG']['rating_success'] = '';
