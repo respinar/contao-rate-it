17e399e9f56d7dc6748da6a81ac38374bc3c6c54
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:25:07 +0330

    Clean up file header in tl_comments

--- contao/dca/tl_comments.php
+++ contao/dca/tl_comments.php
@@ -3,12 +3,12 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
  * @copyright  2019-2020 hofff.com.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
