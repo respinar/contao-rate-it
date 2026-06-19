9cccd6f8c75e7c4f38143a5c1224a1f3482a9565
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:29:01 +0330

    Clean up code style in module DCA

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -3,14 +3,13 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
@@ -21,56 +20,56 @@ use Hofff\Contao\RateIt\EventListener\Dca\ModuleDcaListener;
 $GLOBALS['TL_DCA']['tl_module']['config']['onload_callback'][] = [ModuleDcaListener::class, 'onLoad'];
 $GLOBALS['TL_DCA']['tl_module']['config']['onundo_callback'][] = [ModuleDcaListener::class, 'onRestore'];
 
-/**
+/*
  * palettes
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['rateit']             = '{title_legend},name,rateit_title,type;{rateit_legend},rateit_active;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['rateit'] = '{title_legend},name,rateit_title,type;{rateit_legend},rateit_active;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['rateit_top_ratings'] = '{title_legend},name,headline,type;{rateit_legend},rateit_types,rateit_toptype,rateit_count,rateit_template;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
-/**
+/*
  * fields
  */
 $GLOBALS['TL_DCA']['tl_module']['fields']['rateit_title'] = [
-    'default'   => '',
+    'default' => '',
     'inputType' => 'text',
-    'sql'       => "varchar(255) NOT NULL default ''",
-    'eval'      => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
+    'sql' => "varchar(255) NOT NULL default ''",
+    'eval' => ['mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['rateit_active'] = [
     'inputType' => 'checkbox',
-    'sql'       => "char(1) NOT NULL default ''",
-    'eval'      => ['tl_class' => 'w50 m12'],
+    'sql' => "char(1) NOT NULL default ''",
+    'eval' => ['tl_class' => 'w50 m12'],
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['rateit_types'] = [
-    'inputType'        => 'checkboxWizard',
+    'inputType' => 'checkboxWizard',
     'options_callback' => [ModuleDcaListener::class, 'typeOptions'],
-    'eval'             => ['multiple' => true, 'mandatory' => true],
-    'reference'        => &$GLOBALS['TL_LANG']['tl_module']['rateit_types'],
-    'sql'              => "varchar(255) NOT NULL default ''",
+    'eval' => ['multiple' => true, 'mandatory' => true],
+    'reference' => &$GLOBALS['TL_LANG']['tl_module']['rateit_types'],
+    'sql' => "varchar(255) NOT NULL default ''",
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['rateit_toptype'] = [
-    'default'   => 'best',
+    'default' => 'best',
     'inputType' => 'select',
-    'options'   => ['best', 'most'],
-    'eval'      => ['mandatory' => true, 'tl_class' => 'w50'],
+    'options' => ['best', 'most'],
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
     'reference' => &$GLOBALS['TL_LANG']['tl_module']['rateit_toptype'],
-    'sql'       => "varchar(10) NOT NULL default ''",
+    'sql' => "varchar(10) NOT NULL default ''",
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['rateit_count'] = [
-    'default'   => '10',
+    'default' => '10',
     'inputType' => 'text',
-    'eval'      => ['mandatory' => true, 'maxlength' => 3, 'rgxp' => 'digit', 'tl_class' => 'w50'],
-    'sql'       => "varchar(3) NOT NULL default ''",
+    'eval' => ['mandatory' => true, 'maxlength' => 3, 'rgxp' => 'digit', 'tl_class' => 'w50'],
+    'sql' => "varchar(3) NOT NULL default ''",
 ];
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['rateit_template'] = [
-    'default'          => 'mod_rateit_top_ratings',
-    'inputType'        => 'select',
+    'default' => 'mod_rateit_top_ratings',
+    'inputType' => 'select',
     'options_callback' => [ModuleDcaListener::class, 'getRateItTopModuleTemplates'],
-    'eval'             => ['mandatory' => true, 'tl_class' => 'w50'],
-    'sql'              => "varchar(255) NOT NULL default ''",
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
+    'sql' => "varchar(255) NOT NULL default ''",
 ];
