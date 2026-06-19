a6b834019eed855501f7852f316b1154801e5429
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:24:38 +0330

    Drop author tags and apply coding style

--- contao/dca/tl_article.php
+++ contao/dca/tl_article.php
@@ -3,14 +3,13 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
@@ -20,12 +19,12 @@ use Hofff\Contao\RateIt\EventListener\Dca\ArticleDcaListener;
 
 $GLOBALS['TL_DCA']['tl_article']['config']['onload_callback'][] = [ArticleDcaListener::class, 'onLoad'];
 
-/**
+/*
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_article']['palettes']['__selector__'][] = 'addRating';
 
-/**
+/*
  * Add subpalettes to tl_article
  */
 $GLOBALS['TL_DCA']['tl_article']['subpalettes']['addRating'] = 'rateit_position,rateit_template';
@@ -33,15 +32,15 @@ $GLOBALS['TL_DCA']['tl_article']['subpalettes']['addRating'] = 'rateit_position,
 // Fields
 $GLOBALS['TL_DCA']['tl_article']['fields']['addRating'] = [
     'inputType' => 'checkbox',
-    'sql'       => "char(1) NOT NULL default ''",
-    'eval'      => ['tl_class' => 'w50 m12', 'submitOnChange' => true],
+    'sql' => "char(1) NOT NULL default ''",
+    'eval' => ['tl_class' => 'w50 m12', 'submitOnChange' => true],
 ];
 
 $GLOBALS['TL_DCA']['tl_article']['fields']['rateit_position'] = [
-    'default'   => 'before',
+    'default' => 'before',
     'inputType' => 'select',
-    'options'   => ['after', 'before'],
+    'options' => ['after', 'before'],
     'reference' => &$GLOBALS['TL_LANG']['tl_article'],
-    'sql'       => "varchar(6) NOT NULL default ''",
-    'eval'      => ['mandatory' => true, 'tl_class' => 'w50'],
+    'sql' => "varchar(6) NOT NULL default ''",
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
 ];
