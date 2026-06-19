813b8d45b7646121f86bff6816ca27bb6ca8c912
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:30:01 +0330

    Apply coding style fixes to news DCA

--- contao/dca/tl_news.php
+++ contao/dca/tl_news.php
@@ -3,14 +3,13 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
@@ -18,19 +17,19 @@ declare(strict_types=1);
 
 use Hofff\Contao\RateIt\EventListener\Dca\NewsDcaListener;
 
-/**
+/*
  * Extend tl_article
  */
 
 $GLOBALS['TL_DCA']['tl_news']['config']['onload_callback'][] = [NewsDcaListener::class, 'onLoad'];
 $GLOBALS['TL_DCA']['tl_news']['config']['onundo_callback'][] = [NewsDcaListener::class, 'onUndo'];
 
-/**
+/*
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'][] = 'addRating';
 
-/**
+/*
  * Add subpalettes to tl_article
  */
 $GLOBALS['TL_DCA']['tl_news']['subpalettes']['addRating'] = 'rateit_position';
@@ -38,15 +37,15 @@ $GLOBALS['TL_DCA']['tl_news']['subpalettes']['addRating'] = 'rateit_position';
 // Fields
 $GLOBALS['TL_DCA']['tl_news']['fields']['addRating'] = [
     'inputType' => 'checkbox',
-    'sql'       => "char(1) NOT NULL default ''",
-    'eval'      => ['tl_class' => 'w50 m12', 'submitOnChange' => true],
+    'sql' => "char(1) NOT NULL default ''",
+    'eval' => ['tl_class' => 'w50 m12', 'submitOnChange' => true],
 ];
 
 $GLOBALS['TL_DCA']['tl_news']['fields']['rateit_position'] = [
-    'default'   => 'before',
+    'default' => 'before',
     'inputType' => 'select',
-    'options'   => ['after', 'before'],
+    'options' => ['after', 'before'],
     'reference' => &$GLOBALS['TL_LANG']['tl_news'],
-    'sql'       => "varchar(6) NOT NULL default ''",
-    'eval'      => ['mandatory' => true, 'tl_class' => 'w50'],
+    'sql' => "varchar(6) NOT NULL default ''",
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
 ];
