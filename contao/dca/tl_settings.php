c249b9e11a9bd6c426fa4184774e1f9e03ca2a69
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:35:34 +0330

    Apply code style fixes to DCA files

--- contao/dca/tl_settings.php
+++ contao/dca/tl_settings.php
@@ -3,14 +3,13 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
@@ -18,64 +17,64 @@ declare(strict_types=1);
 
 use Hofff\Contao\RateIt\EventListener\Dca\SettingsDcaListener;
 
-/**
+/*
  * palettes
  */
 $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{rateit_legend:hide},rating_count,rating_textposition'
-    . ',rating_listsize,rating_allow_duplicate_ratings,rating_allow_duplicate_ratings_for_members,rating_template'
-    . ',rating_description,rating_success';
+    .',rating_listsize,rating_allow_duplicate_ratings,rating_allow_duplicate_ratings_for_members,rating_template'
+    .',rating_description,rating_success';
 
-/**
+/*
  * fields
  */
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_count'] = [
-    'default'   => '5',
+    'default' => '5',
     'inputType' => 'select',
-    'options'   => ['1', '5', '10'],
+    'options' => ['1', '5', '10'],
     'reference' => &$GLOBALS['TL_LANG']['tl_settings'],
-    'eval'      => ['mandatory' => true, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_textposition'] = [
-    'default'   => 'after',
+    'default' => 'after',
     'inputType' => 'select',
-    'options'   => ['after', 'before'],
+    'options' => ['after', 'before'],
     'reference' => &$GLOBALS['TL_LANG']['tl_settings'],
-    'eval'      => ['mandatory' => true, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_listsize'] = [
-    'default'   => 10,
+    'default' => 10,
     'inputType' => 'text',
-    'eval'      => ['mandatory' => false, 'maxlength' => 4, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => false, 'maxlength' => 4, 'tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_allow_duplicate_ratings'] = [
     'inputType' => 'checkbox',
-    'eval'      => ['tl_class' => 'clr w50'],
+    'eval' => ['tl_class' => 'clr w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_allow_duplicate_ratings_for_members'] = [
     'inputType' => 'checkbox',
-    'eval'      => ['tl_class' => 'w50'],
+    'eval' => ['tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_template'] = [
-    'default'          => 'rateit_default',
-    'inputType'        => 'select',
+    'default' => 'rateit_default',
+    'inputType' => 'select',
     'options_callback' => [SettingsDcaListener::class, 'getRateItTemplates'],
-    'eval'             => ['mandatory' => true, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => true, 'tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_description'] = [
-    'default'   => '%current%/%max% %type% (%count% [Stimme|Stimmen])',
+    'default' => '%current%/%max% %type% (%count% [Stimme|Stimmen])',
     'inputType' => 'text',
-    'eval'      => ['mandatory' => true, 'allowHtml' => true, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => true, 'allowHtml' => true, 'tl_class' => 'w50'],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['rating_success'] = [
-    'default'   => '%current%/%max% %type% (%count% [Stimme|Stimmen])',
+    'default' => '%current%/%max% %type% (%count% [Stimme|Stimmen])',
     'inputType' => 'text',
-    'eval'      => ['mandatory' => false, 'allowHtml' => true, 'tl_class' => 'w50'],
+    'eval' => ['mandatory' => false, 'allowHtml' => true, 'tl_class' => 'w50'],
 ];
