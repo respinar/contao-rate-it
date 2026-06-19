11e1dc43044c2d0fafe6510c1235d4c544ba06e6
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-06-19 08:35:15 +0330

    Apply code style fixes to DCA files

--- contao/dca/tl_rateit_items.php
+++ contao/dca/tl_rateit_items.php
@@ -3,58 +3,57 @@
 /**
  * This file is part of hofff/contao-rate-it.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
- * @author     David Molineus <david@hofff.com>
- * @author     Carsten Götzinger <info@cgo-it.de>
  * @copyright  2019 hofff.com.
  * @copyright  2013-2018 cgo IT.
  * @license    https://github.com/hofff/contao-rate-it/blob/master/LICENSE LGPL-3.0-or-later
+ *
  * @filesource
  */
 
 declare(strict_types=1);
 
-/**
+/*
  * Table tl_rateit_items
  */
-$GLOBALS['TL_DCA']['tl_rateit_items'] = array(
-    'config' => array(
+$GLOBALS['TL_DCA']['tl_rateit_items'] = [
+    'config' => [
         'dataContainer' => 'Table',
-        'ctable'        => array('tl_rateit_ratings'),
-        'switchToEdit'  => false,
-        'sql'           => array(
-            'keys' => array(
+        'ctable' => ['tl_rateit_ratings'],
+        'switchToEdit' => false,
+        'sql' => [
+            'keys' => [
                 'id' => 'primary',
-            ),
-        ),
-    ),
+            ],
+        ],
+    ],
 
-    'fields' => array(
-        'id'        => array(
-            'sql' => "int(10) unsigned NOT NULL auto_increment",
-        ),
-        'tstamp'    => array(
+    'fields' => [
+        'id' => [
+            'sql' => 'int(10) unsigned NOT NULL auto_increment',
+        ],
+        'tstamp' => [
             'sql' => "int(10) unsigned NOT NULL default '0'",
-        ),
-        'title'     => array(
+        ],
+        'title' => [
             'sql' => "varchar(513) NOT NULL default ''",
-        ),
-        'rkey'      => array(
+        ],
+        'rkey' => [
             'sql' => "varchar(32) NOT NULL default ''",
-        ),
-        'typ'       => array(
+        ],
+        'typ' => [
             'sql' => "varchar(32) NOT NULL default ''",
-        ),
-        'createdat' => array(
+        ],
+        'createdat' => [
             'sql' => "int(10) NOT NULL default '0'",
-        ),
-        'active'    => array(
+        ],
+        'active' => [
             'sql' => "char(1) NOT NULL default ''",
-        ),
-        'parentstatus'    => array(
+        ],
+        'parentstatus' => [
             'sql' => "char(1) NOT NULL default ''",
-        ),
-    ),
-);
+        ],
+    ],
+];
